0afc2165952d363700a001132962f369b587c85f
Dip-Ghosh <dipnsucse@gmail.com>
2021-10-30 12:58:10 +0600

    login page intregate

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,7 +11,7 @@ class PagesController extends Controller
         $page_title = 'Dashboard';
         $page_description = 'Some description for the page';
 
-        return view('pages.dashboard', compact('page_title', 'page_description'));
+        return view('login', compact('page_title', 'page_description'));
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Spatie\Permission\PermissionServiceProvider::class,
         App\Providers\MetronicServiceProvider::class,
 
         /*
